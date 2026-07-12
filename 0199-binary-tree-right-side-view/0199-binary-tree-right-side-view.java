af8cff413a87001608a8a0a36ee0dd46b28f11f6
chinmay-agrwal <chinmayagarwal07@gmail.com>
2026-07-13 00:41:20 +0530

    Time: 0 ms (100%), Space: 43.8 MB (14.8%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -14,34 +14,25 @@
  * }
  */
 class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
-        
-        ArrayList<Integer> ans = new ArrayList<>();
+
+    public void recursive(TreeNode root, int level, ArrayList<Integer> ans){
         if (root == null) {
-            return ans;
+            return;
         }
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
 
-        while (!q.isEmpty()) {
-            ArrayList<Integer> list = new ArrayList<>();
-            int size = q.size();
-            int i = 0;
-
-            for (i = 0; i < size; i++) {
-                TreeNode curr = q.poll();
-                list.add(curr.val);
-
-                if (curr.left != null) {
-                    q.offer(curr.left);
-                }
-                if (curr.right != null) {
-                    q.offer(curr.right);
-                }
-            }
-            int ele = list.get(i-1);
-            ans.add(ele);
+        if (level == ans.size()) {
+            ans.add(root.val);
         }
+
+        recursive(root.right, level+1, ans);
+        recursive(root.left, level+1, ans);
+
+        return;
+    }
+
+    public List<Integer> rightSideView(TreeNode root) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        recursive(root, 0, ans);
         return ans;
     }
 }
