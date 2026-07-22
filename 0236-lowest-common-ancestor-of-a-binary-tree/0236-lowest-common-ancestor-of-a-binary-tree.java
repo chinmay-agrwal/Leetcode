f30c76c82b931a104d15114db64349fbcebcf784
chinmay-agrwal <chinmayagarwal07@gmail.com>
2026-07-22 16:20:33 +0530

    Time: 30 ms (6.05%), Space: 80.3 MB (6.89%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,38 +9,38 @@
  */
 class Solution {
 
-    public boolean dfs(TreeNode root, ArrayList<Integer> list, TreeNode x){
+    public boolean dfs(TreeNode root, ArrayList<TreeNode> list, TreeNode target){
         if (root == null) {
             return false;
         }
 
-        list.add(root.val);
+        list.add(root);
 
-        if (root.val == x.val) {
+        if (root.val == target.val) {
             return true;
         }
-        if (dfs(root.left, list, x) || dfs(root.right, list, x)) {
+
+        if (dfs(root.left, list, target) || dfs(root.right, list, target)) {
             return true;
         }
 
         list.remove(list.size()-1);
+
         return false;
     }
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         
-        ArrayList<Integer> list1 = new ArrayList<>();
-        ArrayList<Integer> list2 = new ArrayList<>();
+        ArrayList<TreeNode> list1 = new ArrayList<>();
+        ArrayList<TreeNode> list2 = new ArrayList<>();
 
         dfs(root, list1, p);
         dfs(root, list2, q);
 
         int i = 0;
-        int ind = 0;
-        
-        while (i < list1.size() && i <list2.size()) {
+
+        while (i<list1.size() && i<list2.size()) {
             if (list1.get(i).equals(list2.get(i))) {
-                ind = i;
                 i++;
             }
             else{
@@ -48,6 +48,6 @@ class Solution {
             }
         }
 
-        return new TreeNode(list1.get(ind));
+        return list1.get(i-1);
     }
 }
