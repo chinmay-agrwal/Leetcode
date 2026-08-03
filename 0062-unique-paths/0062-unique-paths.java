0a09d7683332fd93c515b12e62040554dd6047ce
chinmay-agrwal <chinmayagarwal07@gmail.com>
2026-08-04 03:17:41 +0530

    Time: 0 ms (100%), Space: 41.9 MB (83.28%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int memo(int m, int n, int dp[][]){
+        if (m==0 && n==0) {
+            return 1;
+        }
+        if (m<0 || n<0) {
+            return 0;
+        }
+        if (dp[m][n] != -1) {
+            return dp[m][n];
+        }
+
+        int h = memo(m, n-1, dp);
+        int v = memo(m-1, n, dp);
+
+        return dp[m][n] = h+v;
+    }
+
+    public int uniquePaths(int m, int n) {
+        
+        int dp[][] = new int[m][n];
+        for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        int ans = memo(m-1, n-1, dp);
+        return ans;
+
+    }
+}
